1a9c6e710389f81f20ee382ca07ff24adc5b8bca
Akenore <joudakenore@gmail.com>
2026-01-07 15:41:12 +0100

    Initial Commit

---
+++ FielmedinaTests/FielmedinaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FielmedinaTests.swift
+//  FielmedinaTests
+//
+//  Created by Aslan on 1/7/26.
+//
+
+import Testing
+@testable import Fielmedina
+
+struct FielmedinaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
