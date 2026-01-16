4557dd548ad0b66f24de464961c2cc855b0e5ce4
Akenore <joudakenore@gmail.com>
2026-01-16 12:24:51 +0100

    feat: Implement environment-based URL configuration for ApolloClient.

--- Fielmedina/Services/API/ApolloClient.swift
+++ Fielmedina/Services/API/ApolloClient.swift
@@ -12,13 +12,29 @@ import ApolloAPI
 class Network {
     static let shared = Network()
     
+    enum Environment {
+        case production
+        case development
+        
+        var baseURL: URL {
+            switch self {
+            case .production:
+                return URL(string: "https://mystory.fielmedina.com/graphql")!
+            case .development:
+                return URL(string: "https://mystory.fielmedina.com/graphql")!
+            }
+        }
+    }
+    
+    static let currentEnvironment: Environment = .production
+    
     private(set) lazy var apollo: ApolloClient = {
-        let url = URL(string: "https://mystory.fielmedina.com/graphql")!
         let store = ApolloStore()
         
         let sessionConfiguration = URLSessionConfiguration.default
         sessionConfiguration.timeoutIntervalForRequest = 30.0
         let urlSession = URLSession(configuration: sessionConfiguration)
-        return ApolloClient(url: url)
+        
+        return ApolloClient(url: Network.currentEnvironment.baseURL)
     }()
 }
