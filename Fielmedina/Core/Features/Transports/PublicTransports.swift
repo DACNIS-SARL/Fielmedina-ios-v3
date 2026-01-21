4b683aaa42dd8ef7436a6329bbbfd789d0a4147b
Akenore <joudakenore@gmail.com>
2026-01-21 11:26:54 +0100

    feat: Implement programmatic navigation for home features, add public transport and taxi booking views, and introduce emergency contact functionality.

---
+++ Fielmedina/Core/Features/Transports/PublicTransports.swift
@@ -0,0 +1,15 @@
+//
+//  TransportsList.swift
+//  Fielmedina
+//
+//  Created by Aslan on 1/21/26.
+//
+
+import SwiftUI
+
+
+struct PublicTransportList: View {
+    var body: some View {
+       Text("Public Transport list")
+    }
+}
